b7b891851a20f1596d24f5bb9c90acddf731f21a
sasddu0 <siddu123raja@gmail.com>
2024-02-28 13:08:29 -0600

    Initial commit - files created

---
+++ VoiceChatBot-VisionPro/VoiceChatBot_VisionProApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceChatBot_VisionProApp.swift
+//  VoiceChatBot-VisionPro
+//
+//  Created by Siddanth Raja on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceChatBot_VisionProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
