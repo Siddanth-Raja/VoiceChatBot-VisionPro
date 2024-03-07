527e42bbe55ec50cf29d21dc9c9bfd5933a8920f
sasddu0 <siddu123raja@gmail.com>
2024-03-07 09:06:33 -0600

    Made another view for options to do something with the text, Added openAI API for summarize and translation (not working yet), Added .gitignore file

---
+++ VoiceChatBot-VisionPro/OptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionsView.swift
+//  VoiceChatBot-VisionPro
+//
+//  Created by Siddanth Raja on 3/6/24.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionsView()
+}
